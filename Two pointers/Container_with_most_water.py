4d233af2dcc17f973022f27fde1d7dd84c5a443c
Harshmeet Singh Chandhok <66329612+harshmeet30@users.noreply.github.com>
2025-08-17 16:53:25 +1000

    Update Container_with_most_water.py

--- Two pointers/Container_with_most_water.py
+++ Two pointers/Container_with_most_water.py
@@ -11,3 +11,13 @@ class Solution:
                 maxArea = max(maxArea, min(height[l],height[r])*(r-l))
                 r-=1
         return maxArea
+
+
+# Brute Force -
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         maxarea=0
+#         for i in range(len(height)):
+#             for j in range(i+1,len(height)):
+#                     maxarea = max(maxarea, (min(height[i],height[j]))*(j-i) )
+#         return maxarea
